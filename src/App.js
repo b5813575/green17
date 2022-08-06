2f21f6055e5d0a91a86c0a33b22a110a32e34920
iss5813575 <ihsan.s@psu.ac.th>
2022-08-07 04:54:53 +0700

    2

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
+function App() {
+  return (
+    <div className="App">
+      <header className="responsive-iframe">
+        <iframe
+          className="responsive-iframe"
+          title="Green office"
+          position="absolute"
+          frameBorder="0"
+          src="https://app.powerbi.com/view?r=eyJrIjoiYmI1MTlhNjYtY2FjYy00NzM4LWIzZDgtY2QzOThlYzFmNDZjIiwidCI6IjhlNjM0ZTY3LTlkNjYtNDZkMi1hNTI5LWUxYjcwOGM1ZDhiYyIsImMiOjEwfQ%3D%3D"
+        ></iframe>
+      </header>
+    </div>
+  );
+}
+
+export default App;
